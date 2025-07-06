7b3384c5ab5eefb9e5bedcbc97a489b2ea9833a3
Vaibhav <vaibhavchaubey786@gmail.com>
2025-07-06 13:13:43 +0530

    initialize user microservice with Spring Boot

---
+++ src/main/java/com/hms/user/UserMicroserviceHmsApplication.java
@@ -0,0 +1,13 @@
+package com.hms.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroserviceHmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroserviceHmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hms/user/UserMicroserviceHmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceHmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
