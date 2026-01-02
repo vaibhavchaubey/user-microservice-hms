b8d79f520a846f6d6a6d68b9a5169c92e8e76b30
Vaibhav <vaibhavchaubey786@gmail.com>
2026-01-02 15:25:03 +0530

    Remove unused imports in LoginDTO

--- src/main/java/com/hms/user/dto/LoginDTO.java
+++ src/main/java/com/hms/user/dto/LoginDTO.java
@@ -1,11 +1,5 @@
 package com.hms.user.dto;
 
-import com.hms.user.entity.User;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
